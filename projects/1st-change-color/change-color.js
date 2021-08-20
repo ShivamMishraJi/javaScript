92a918733425140dcf40a7280c05a11d62d32a1f
Shivam Mishra <shivam199442@gmail.com>
2021-08-20 19:57:35 +0530

    1st project of change color in javaScript

---
+++ projects/1st-change-color/change-color.js
@@ -0,0 +1,20 @@
+var buttonClick = document.getElementById("clickMeButton");
+buttonClick.addEventListener("click", changeBodyColor);
+
+function changeBodyColor() {
+//   console.log("inside changeBodyColor method");
+  var color = getRandomColor();
+  document.body.style.background = color;
+}
+
+// Math.random() returns a random number between 0 (inclusive),  and 1 (exclusive):
+// Math.floor() - Round a number downward to its nearest integer:
+function getRandomColor() {
+//   console.log("inside changeBodyColor method");
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
