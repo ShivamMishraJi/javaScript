904ec816fb5a4854cfd94b0ba1e6fc87338e9f38
Shivam Mishra <shivam199442@gmail.com>
2021-09-17 22:33:21 +0530

    made small changes

--- projects/33rd-background-color-switch/app.js
+++ projects/33rd-background-color-switch/app.js
@@ -1,22 +1,20 @@
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
-console.log(buttons);
-
 buttons.forEach(function(button){
     button.addEventListener('click', function(e){
-        console.log(e.target);
         if (e.target.id === 'grey'){
-            body.style.backgroundColor = e.target.id;
+            body.style.backgroundColor = 'grey';
         }
         if (e.target.id === 'white'){
-            body.style.backgroundColor = e.target.id;
+            body.style.backgroundColor = 'white';
         }
         if (e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id;
+            body.style.backgroundColor = 'blue';
         }
         if (e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id;
+            body.style.backgroundColor = 'yellow';
+            // body.style.backgroundColor = e.target.id;
         }
     })
 })
